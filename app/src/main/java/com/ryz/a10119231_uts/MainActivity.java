5f078611bf2ce805578df51827e940c55712a179
MuhamadZulhaqi24 <muhamadzulhaqi0@gmail.com>
2022-06-03 18:08:42 +0700

    Initial commit

---
+++ app/src/main/java/com/ryz/a10119231_uts/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ryz.a10119231_uts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+/** NIM   : 10119231
+ * Nama   : Muhamad Zulhaqi
+ * Kelas  : IF-6
+ * Tanggal Pengerjaan : 06-05-2022
+ */
